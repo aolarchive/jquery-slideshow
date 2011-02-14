2f9ab7580d98f1bceca0354083efaaf94715be46
avayanis <avayanis@32af4f46-d4c3-4f50-86b0-8ee09f626b3f>
2011-02-14 18:52:11 +0000

    Adding options documentation

--- plugins/gallery-management.js
+++ plugins/gallery-management.js
@@ -1,12 +1,13 @@
 <blogsmith:box>
 	<blogsmith:title>AOL Photo Gallery Media Button Override</blogsmith:title>
 	<blogsmith:body>
-		<script>
-			$(function() {
+		<script>		
+			$(function() {			
 				var $configure = $("#aol-photo-gallery-configuration"),
 					$container = $configure.parent().parent(),
 					$metaKeys = $(blogsmith.listMetaDataKeys()),
 					$configForm = $('<dl>'),
+					writeToEditorOriginal = writeToEditor,
 					metaData = blogsmith.getMetaData(),
 					galleryMetaKeys = { "GallerySponsorName" : "Sponsor name.",
 										"GallerySponsorURL" : "Sponsorship URL target.",
@@ -14,6 +15,14 @@
 										"GalleryAdRefreshMN" : "Magic number for gallery ad.",
 										"GalleryAdRefreshCount" : "Number of gallery clicks between ad refresh."},
 					contents;
+
+				// Override default insert gallery button
+				writeToEditor = function(text) {
+					if (/%Gallery-\d+%/.test(text))
+						text = text.replace(/Gallery/, 'VIRTUAL-Gallery');
+
+					writeToEditorOriginal(text);
+				};
 				
 				// Initalize plugin -	Hide plugin.  Create configuration form. 
 				//						Search for missing meta data keys and add if needed.
